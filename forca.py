dd67d32e6827478d048bb32c279d7a49a2fc50e0
Allan <allansilvaoliveira@outlook.com>
2022-12-16 15:52:11 -0300

    att 1.1

--- forca.py
+++ forca.py
@@ -1,27 +1,47 @@
 def jogar():
     print('Jogando....')
 
-    palavra = 'banana'
+    palavra = 'python'.upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while not enforcou and not acertou:
         chute = input('Qual a letra ?')
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         letra = palavra.find(chute)
 
-        index = 0
-        
-        for letra in palavra:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index = index + 1
-            print(letras_acertadas)
-            
-                
+
+
+        if chute in palavra:
+            index = 0
+            for letra in palavra:
+                if chute.upper() == letra.upper():
+                    letras_acertadas[index] = letra
+                index += 1
+                print(letras_acertadas)
+
+
+        else:
+
+            erros += 1
+
+        enforcou = erros == 6
+
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+
+    if acertou:
+        print('Parabens! Você ganhou ") ')
+    else:
+        print('Não desanime, tente outra vez. ')
+
+    print("FIM DO JOGO")
+
 
 if __name__ == '__main__':
     jogar()
