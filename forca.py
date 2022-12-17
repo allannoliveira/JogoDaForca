b777bb7189674f7992a729c43ffe19e8a5bfa9fc
Allan Oliveira <allansilvaoliveira@outlook.com>
2022-12-16 23:42:24 -0300

    att 0.01

--- forca.py
+++ forca.py
@@ -1,24 +1,42 @@
+import random
 def jogar():
     print('Jogando....')
 
-    palavra = 'python'.upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavras = palavras[numero].upper()
+
+
+    letras_acertadas = ['_' for letra in palavras]
+
+
+    
 
     enforcou = False
     acertou = False
     erros = 0
 
+    print(letras_acertadas)
     while not enforcou and not acertou:
         chute = input('Qual a letra ?')
         chute = chute.strip().upper()
 
-        letra = palavra.find(chute)
+        letra = palavras.find(chute)
 
 
-
-        if chute in palavra:
+       
+        if chute in palavras:
             index = 0
-            for letra in palavra:
+            for letra in palavras:
                 if chute.upper() == letra.upper():
                     letras_acertadas[index] = letra
                 index += 1
